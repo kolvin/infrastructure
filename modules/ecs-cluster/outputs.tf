6918a6bfd4dda6be6245544c6a59850f6f31fe05
Kolvin <taylorcalvin07@gmail.com>
2021-07-26 21:17:10 +0100

    docs: using inline docblocks to generate terraform-docs, added version.tf to enforce versions, generated README for module

--- modules/ecs-cluster/outputs.tf
+++ modules/ecs-cluster/outputs.tf
@@ -1,18 +1,23 @@
-output "cluster_id" {
+/**
+ * # License
+ * GNU GENERAL PUBLIC LICENSE. See [LICENSE](../../LICENSE) for full details.
+ */
+
+output "id" {
   value       = aws_ecs_cluster.this.id
   sensitive   = false
   description = "ID of this ecs cluster"
   depends_on  = []
 }
 
-output "cluster_arn" {
+output "arn" {
   value       = aws_ecs_cluster.this.arn
   sensitive   = false
   description = "ARN of this ecs cluster"
   depends_on  = []
 }
 
-output "cluster_name" {
+output "name" {
   value       = aws_ecs_cluster.this.name
   sensitive   = false
   description = "Name of this ecs cluster"
