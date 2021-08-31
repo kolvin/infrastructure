2aad1561714dc567105b3e112728c4cf0390ed62
Kolvin <taylorcalvin07@gmail.com>
2021-08-31 14:33:24 +0100

    docs: cleanup

--- modules/ecs-service/outputs.tf
+++ modules/ecs-service/outputs.tf
@@ -1,9 +1,11 @@
 output task_definition_arn {
-  description = "The ARN of the service task definition"
+  sensitive   = false
   value = aws_ecs_task_definition.this.arn
+  description = "The ARN of the service task definition"
 }
 
 output service_id {
-  description = "The ID of the service"
+  sensitive   = false
   value = aws_ecs_service.this.id
+  description = "The ID of the service"
 }
