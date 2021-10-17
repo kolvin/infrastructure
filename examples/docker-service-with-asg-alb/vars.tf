9f225bbfda3c617a0af0dcb5b4b3008c6ad34ab5
Kolvin <taylorcalvin07@gmail.com>
2021-10-17 03:46:59 +0100

    qa: applied fmt

--- examples/docker-service-with-asg-alb/vars.tf
+++ examples/docker-service-with-asg-alb/vars.tf
@@ -1,35 +1,35 @@
-variable region {
-  default = "eu-west-1"
-  type = string
+variable "region" {
+  default     = "eu-west-1"
+  type        = string
   description = "AWS Region context"
 }
 
-variable profile {
-  default = "personal"
-  type = string
+variable "profile" {
+  default     = "personal"
+  type        = string
   description = "Credentials Profile name"
 }
 
-variable environment {
-  default = "foo"
-  type = string
+variable "environment" {
+  default     = "foo"
+  type        = string
   description = "Environment name identifier"
 }
 
-variable product_name {
-  default = "bar"
-  type = string
+variable "product_name" {
+  default     = "bar"
+  type        = string
   description = "Service Name"
 }
 
-variable creator {
-  default = "Terraform"
-  type = string
+variable "creator" {
+  default     = "Terraform"
+  type        = string
   description = "Who made me"
 }
 
-variable cdir_prefix {
-  default = "11.120"
-  type = string
+variable "cdir_prefix" {
+  default     = "11.120"
+  type        = string
   description = "Prefix to use for VPC creation"
 }
