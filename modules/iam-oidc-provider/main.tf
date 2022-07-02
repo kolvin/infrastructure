b155ea1573374b3c103a8443b929a2601047a760
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-07-02 22:33:39 +0100

    debug(ci): reverted changes on modules, rename module workflows
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

--- modules/iam-oidc-provider/main.tf
+++ modules/iam-oidc-provider/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "this" {
   url             = var.provider_config.url
-  thumbprint_list = var.provider_config.thumbprint_list
   client_id_list  = var.provider_config.client_id_list
+  thumbprint_list = var.provider_config.thumbprint_list
 }
